c5bd5e464a4e0a3235ab0380acc076e6982ed268
hielf <jerrymou.dev@gmail.com>
2025-06-16 13:34:41 -0700

    Initial commit: Set up Rails project with RSpec, custom error pages, and .gitignore configuration.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    # This action can be left empty if you don't need any specific logic here.
+  end
+end 
---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+# Define the root route
+  root to: 'home#index'
+
+  match '/404', to: 'errors#not_found', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
+
+  # Catch-all route to redirect all exceptions to the 404 page
+  match '*path', to: 'errors#not_found', via: :all
+end
